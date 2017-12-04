c11ff2e7d7342d52cc6ca67e76a94ad633095692
PharuS <pharus@mail.ru>
2017-12-04 03:37:32 +0300

    add Property File application spring context

--- src/main/java/quoters/TermitatorQuoter.java
+++ src/main/java/quoters/TermitatorQuoter.java
@@ -8,6 +8,10 @@ import javax.annotation.PostConstruct;
 @Profiling
 @DeprecatedClass(newImpl = T000.class)
 public class TermitatorQuoter implements Quoter {
+    public void setRepeat(int repeat) {
+        this.repeat = repeat;
+    }
+
     @InjectRandomInt(min = 2 , max = 7)
     private int repeat;
 

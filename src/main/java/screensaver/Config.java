bbebd78813587f61a85d3e04fc1d9ee13841d9b8
PharuS <pharus@mail.ru>
2017-12-04 04:07:29 +0300

    abstract class for prototype

--- src/main/java/screensaver/Config.java
+++ src/main/java/screensaver/Config.java
@@ -9,18 +9,28 @@ import java.util.Random;
 @ComponentScan(basePackages = "screensaver")
 public class Config {
     @Bean
-    @Scope(value = "prototype", proxyMode = ScopedProxyMode.TARGET_CLASS)
+    @Scope(value = "prototype")
     public Color color(){
         Random random = new Random();
         return new Color(random.nextInt(255),random.nextInt(255),random.nextInt(255));
 
     }
 
+    @Bean
+    public ColorFrame frame(){
+        return new ColorFrame() {
+            @Override
+            public Color getColor() {
+                return color();
+            }
+        };
+    }
+
     public static void main(String[] args) throws InterruptedException {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Config.class);
         while (true){
             context.getBean(ColorFrame.class).showOnRandomPlase();
-            Thread.sleep(100);
+            Thread.sleep(200);
         }
 
     }
